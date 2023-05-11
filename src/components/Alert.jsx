df3a3cf06c465f761b848fab356c5a324ac08e76
Paramveer Singh <piscesparamveer@gmail.com>
2023-05-12 00:19:54 +0530

    Update Alert.jsx

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Alert(props) {
     return (
         props.msg && <div className={`alert alert-${props.type} alert-dismissible`} role="alert">
-      <strong>{props.msg}</strong>
+      {props.msg}
     </div>
     )
 }
